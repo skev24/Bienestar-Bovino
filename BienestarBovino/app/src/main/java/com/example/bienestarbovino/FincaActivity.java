9305714bc9736ea9df8058653e024ab1b3e014bd
skev24 <gkeving24@gmail.com>
2022-04-07 20:59:07 -0600

    Se agregan transiciones

--- BienestarBovino/app/src/main/java/com/example/bienestarbovino/FincaActivity.java
+++ BienestarBovino/app/src/main/java/com/example/bienestarbovino/FincaActivity.java
@@ -2,13 +2,32 @@ package com.example.bienestarbovino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FincaActivity extends AppCompatActivity {
+
+    private Button btnMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.finca);
+
+        btnMenu = findViewById(R.id.buttonContinueFinca);
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenu(view);
+            }
+        });
+    }
+
+    public void openMenu(View view){
+        Intent intent = new Intent(FincaActivity.this, MenuActivity.class);
+        startActivity(intent);
     }
 }
