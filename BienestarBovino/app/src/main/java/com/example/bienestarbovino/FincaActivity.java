aaf092f7bd506ecf2239904c48f08da040a8156b
skev24 <gkeving24@gmail.com>
2022-04-07 20:52:17 -0600

    Revert "Se agregan transiciones"
    This reverts commit 85090b1ada2cd1bb2fb93b9073370df10252ed82.

--- BienestarBovino/app/src/main/java/com/example/bienestarbovino/FincaActivity.java
+++ BienestarBovino/app/src/main/java/com/example/bienestarbovino/FincaActivity.java
@@ -2,34 +2,13 @@ package com.example.bienestarbovino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class FincaActivity extends AppCompatActivity {
-
-    private Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.finca);
-
-        btnContinue = findViewById(R.id.buttonContinueFinca);
-
-        btnContinue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openMenu(view);
-            }
-
-        });
-
-
-    }
-    public void openMenu(View view){
-        Intent intent = new Intent(FincaActivity.this, MenuActivity.class);
-        startActivity(intent);
     }
 }
