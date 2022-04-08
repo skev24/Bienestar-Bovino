96e568e6b6ed2be48f77ddb51c97a7172c2b786a
skev24 <gkeving24@gmail.com>
2022-04-07 21:30:32 -0600

    Pequeños cambios

--- BienestarBovino/app/src/main/java/com/example/bienestarbovino/FincaActivity.java
+++ BienestarBovino/app/src/main/java/com/example/bienestarbovino/FincaActivity.java
@@ -6,15 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import model.finca;
 
 public class FincaActivity extends AppCompatActivity {
 
     private Button btnMenu;
+    private EditText nameFinca, tamFinca;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    model.finca newFinca;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.finca);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        nameFinca = findViewById(R.id.editTextTextFincaName);
+        tamFinca = findViewById(R.id.editTextTextFincaSize);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("finca");
+
+        newFinca = new finca();
 
         btnMenu = findViewById(R.id.buttonContinueFinca);
 
@@ -27,7 +51,27 @@ public class FincaActivity extends AppCompatActivity {
     }
 
     public void openMenu(View view){
+
+        // getting text from our edittext fields.
+        String nameFinca = name.getText().toString();
+        String phone = employeePhoneEdt.getText().toString();
+        String address = employeeAddressEdt.getText().toString();
+
+        // below line is for checking weather the
+        // edittext fields are empty or not.
+        if (TextUtils.isEmpty(name) && TextUtils.isEmpty(phone) && TextUtils.isEmpty(address)) {
+            // if the text fields are empty
+            // then show the below message.
+            Toast.makeText(MainActivity.this, "Please add some data.", Toast.LENGTH_SHORT).show();
+        } else {
+            // else call the method to add
+            // data to our database.
+            addDatatoFirebase(name, phone, address);
+        }
         Intent intent = new Intent(FincaActivity.this, MenuActivity.class);
         startActivity(intent);
+
+        //Toast.makeText(getApplicationContext(), mAuth.getCurrentUser().getUid(),
+          //      Toast.LENGTH_SHORT).show();
     }
 }
---
+++ BienestarBovino/app/src/main/java/model/finca.java
@@ -0,0 +1,35 @@
+package model;
+
+public class finca {
+    private String name;
+    private String tam;
+    private String user;
+
+    public finca(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTam() {
+        return tam;
+    }
+
+    public void setTam(String tam) {
+        this.tam = tam;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
