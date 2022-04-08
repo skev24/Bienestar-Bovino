7ed283eb8cb9c4dde65f46662d363d9e112adc79
skev24 <gkeving24@gmail.com>
2022-04-08 01:43:36 -0600

    pequeños cambios

--- BienestarBovino/app/src/main/java/com/example/bienestarbovino/FincaActivity.java
+++ BienestarBovino/app/src/main/java/com/example/bienestarbovino/FincaActivity.java
@@ -1,17 +1,24 @@
 package com.example.bienestarbovino;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 import model.finca;
 
@@ -20,23 +27,18 @@ public class FincaActivity extends AppCompatActivity {
     private Button btnMenu;
     private EditText nameFinca, tamFinca;
 
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
-    model.finca newFinca;
     private FirebaseAuth mAuth;
+    private finca newFinca;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.finca);
 
-        mAuth = FirebaseAuth.getInstance();
-
         nameFinca = findViewById(R.id.editTextTextFincaName);
         tamFinca = findViewById(R.id.editTextTextFincaSize);
 
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference("finca");
+        mAuth = FirebaseAuth.getInstance();
 
         newFinca = new finca();
 
@@ -45,33 +47,16 @@ public class FincaActivity extends AppCompatActivity {
         btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openMenu(view);
+                createFinca(view);
             }
         });
     }
 
-    public void openMenu(View view){
+    public void createFinca(View view){
 
-        // getting text from our edittext fields.
-        String nameFinca = name.getText().toString();
-        String phone = employeePhoneEdt.getText().toString();
-        String address = employeeAddressEdt.getText().toString();
-
-        // below line is for checking weather the
-        // edittext fields are empty or not.
-        if (TextUtils.isEmpty(name) && TextUtils.isEmpty(phone) && TextUtils.isEmpty(address)) {
-            // if the text fields are empty
-            // then show the below message.
-            Toast.makeText(MainActivity.this, "Please add some data.", Toast.LENGTH_SHORT).show();
-        } else {
-            // else call the method to add
-            // data to our database.
-            addDatatoFirebase(name, phone, address);
-        }
         Intent intent = new Intent(FincaActivity.this, MenuActivity.class);
         startActivity(intent);
-
-        //Toast.makeText(getApplicationContext(), mAuth.getCurrentUser().getUid(),
-          //      Toast.LENGTH_SHORT).show();
     }
+
+
 }
--- BienestarBovino/app/src/main/java/com/example/bienestarbovino/VetMenuActivity.java
+++ BienestarBovino/app/src/main/java/com/example/bienestarbovino/VetMenuActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class VetMenuActivity extends AppCompatActivity {
 
-    private Button buttonVacunaClass;
+    private ImageButton buttonVacunaClass;
 
     protected void onCreate(Bundle savedInstanceState) {
 
