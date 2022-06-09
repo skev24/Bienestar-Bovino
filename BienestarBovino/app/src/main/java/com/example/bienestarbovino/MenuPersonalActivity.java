a02a98e8106b74123e29f2b4410f7fe06f88692d
nakidixon <nakidixon@gmail.com>
2022-06-09 13:03:14 -0600

    solucion errores tratamiento

--- BienestarBovino/app/src/main/java/com/example/bienestarbovino/MenuPersonalActivity.java
+++ BienestarBovino/app/src/main/java/com/example/bienestarbovino/MenuPersonalActivity.java
@@ -1,33 +1,15 @@
 package com.example.bienestarbovino;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.icu.util.Calendar;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.ImageButton;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import control.Funciones;
-import model.Tarea;
 
 public class MenuPersonalActivity extends AppCompatActivity {
 
---
+++ BienestarBovino/app/src/main/java/model/tarea.java
@@ -0,0 +1,57 @@
+package model;
+
+public class tarea {
+    private String encargadoTarea;
+    private String descripcion;
+    private String fecha; //cambiar por date
+    private boolean concluida;
+    private String idFinca;
+
+    public tarea(String fecha, String encargadoTarea, String descripcion, boolean concluida, String idFinca) {
+        this.encargadoTarea = encargadoTarea;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.concluida = concluida;
+        this.idFinca = idFinca;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setIdFinca(String idFinca){
+        this.idFinca = idFinca;
+    }
+
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setConcluida(boolean concluida){
+        this.concluida = concluida;
+    }
+
+    public void setEncargadoTarea(String encargadoTarea) {
+        this.encargadoTarea = encargadoTarea;
+    }
+
+    public String getfecha() {
+        return fecha;
+    }
+
+    public String getIdFinca(){return idFinca;}
+
+
+    public String getEncargadoTarea() {
+        return encargadoTarea;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public  boolean getConcluida(){
+        return concluida;
+    }
+}
